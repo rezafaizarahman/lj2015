2a2ee87602d5741c1c744ac6aad0902dc9fa878a
Reza Faiz A Rahman <rezafaizarahman@gmail.com>
2015-06-01 07:42:56 +0700

    update: 01/06/2015

--- Gruntfile.js
+++ Gruntfile.js
@@ -301,7 +301,13 @@ module.exports = function (grunt) {
           cwd: 'bower_components/bootstrap/dist',
           src: 'fonts/*',
           dest: '<%= config.dist %>'
-        }]
+      }, {
+          expand: true,
+          dot: true,
+          cwd: 'bower_components/font-awesome',
+          src: 'fonts/*.*',
+          dest: '<%= config.dist %>'
+      }]
       },
       styles: {
         expand: true,
